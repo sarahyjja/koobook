b0c1ace411f48dbbd4c3787998a8cea51f336973
sarah <sarahkharraz@gmail.com>
2019-01-24 19:20:31 +0000

    index.js modification10

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // if (process.env.NODE_ENV == 'development') require('dotenv').config({ silent: true });
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/'));
 
 app.get((req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
