9aac878343285e2eef369160899f790a0db32764
sarah <sarahkharraz@gmail.com>
2019-01-24 16:34:35 +0000

    Heroku

---
+++ myscript.js
@@ -0,0 +1,22 @@
+async function bookSearch(){
+  const search = await document.getElementById('search').value
+  document.getElementById('results').innerHTML = "";
+
+  $.ajax({
+    url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
+    dataType: "json",
+    success: function(data){
+      for(i = 0; i < data.items.length; i++){
+        results.innerHTML += `<img id=image src=${data.items[i].volumeInfo.imageLinks.smallThumbnail}/>`
+        results.innerHTML += `<h3 id=text>${data.items[i].volumeInfo.title}</h3>`
+        results.innerHTML += `<h3 id=text>By ${data.items[i].volumeInfo.authors}</h3>`
+        results.innerHTML += `<h4 id=text>Published by ${data.items[i].volumeInfo.publisher}</h4>`
+        results.innerHTML += `<p id=text>${data.items[i].volumeInfo.description}</p>`
+        results.innerHTML += `<a href=${data.items[i].volumeInfo.infoLink} id=text>Read more...<br></a>`
+      }
+    },
+    type: 'GET'
+  });
+}
+
+document.getElementById('submit').addEventListener('click', bookSearch, false)
