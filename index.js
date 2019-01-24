1ab78defea011377db54bc39dcaaf83cfd934456
sarah <sarahkharraz@gmail.com>
2019-01-24 19:22:49 +0000

    index.js modification11

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 const app = express();
 
-// if (process.env.NODE_ENV == 'development') require('dotenv').config({ silent: true });
-
 app.use(express.static(__dirname + '/'));
 
 app.get((req, res) => {
@@ -13,74 +11,3 @@ app.get((req, res) => {
 });
 
 app.listen(port);
-
-// ----------ok under
-// const http = require('http');
-// const express = require('express');
-// const PORT = process.env.PORT || 5000;
-// // const app = new express();
-// var app = express();
-// app.get('/', function(request, response){
-//     response.sendfile('index.html');
-// });
-// app.listen(PORT, () => {
-//   console.log(`Server running on ${PORT}/`);
-// });
-
-// -------
-// var spawn = require('child_process').spawn;
-// var path = require('path');
-// var fs = require('fs');
-//
-// function installArchSpecificPackage(version, require) {
-//
-//   process.env.npm_config_global = 'false';
-//
-//   var platform = process.platform == 'win32' ? 'win' : process.platform;
-//   var arch = platform == 'win' && process.arch == 'ia32' ? 'x86' : process.arch;
-//
-//   var cp = spawn(platform == 'win' ? 'npm.cmd' : 'npm', ['install', '--no-save', ['node', platform, arch].join('-') + '@' + version], {
-//     stdio: 'inherit',
-//     shell: true
-//   });
-//
-//   cp.on('close', function(code) {
-//     var pkgJson = require.resolve(['node', platform, arch].join('-') + '/package.json');
-//     var subpkg = JSON.parse(fs.readFileSync(pkgJson, 'utf8'));
-//     var executable = subpkg.bin.node;
-//     var bin = path.resolve(path.dirname(pkgJson), executable);
-//
-//     try {
-//       fs.mkdirSync(path.resolve(process.cwd(), 'bin'));
-//     } catch (e) {
-//       if (e.code != 'EEXIST') {
-//         throw e;
-//       }
-//     }
-//
-//     linkSync(bin, path.resolve(process.cwd(), executable));
-//
-//     if (platform == 'win') {
-//       var pkg = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json')));
-//       fs.writeFileSync(path.resolve(process.cwd(), 'bin/node'), 'This file intentionally left blank');
-//       pkg.bin.node = 'bin/node.exe';
-//       fs.writeFileSync(path.resolve(process.cwd(), 'package.json'), JSON.stringify(pkg, null, 2));
-//     }
-//
-//     return process.exit(code);
-//
-//   });
-// }
-//
-// function linkSync(src, dest) {
-//   try {
-//     fs.unlinkSync(dest);
-//   } catch (e) {
-//     if (e.code != 'ENOENT') {
-//       throw e;
-//     }
-//   }
-//   return fs.linkSync(src, dest);
-// }
-//
-// module.exports = installArchSpecificPackage;
