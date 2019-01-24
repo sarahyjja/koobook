f403e932446ff65093deaa6d70d77b1aad1089c3
sarah <sarahkharraz@gmail.com>
2019-01-24 18:30:40 +0000

    index.js modification4

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const http = require('http');
 const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(html);
-});
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
-});
+const express = require('express');
+const app = new express();
 
+app.get('/', function(request, response){
+    response.sendfile('index.html');
+});
+//
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.write(data)
+//   res.end(html);
+// });
+// server.listen(PORT, () => {
+//   console.log(`Server running on ${PORT}/`);
+// });
 
+// -------
 // var spawn = require('child_process').spawn;
 // var path = require('path');
 // var fs = require('fs');
