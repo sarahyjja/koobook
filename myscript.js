c921bde165df53be9e376247223d65b833596cbe
sarah <sarahkharraz@gmail.com>
2019-01-28 22:13:10 +0000

    Deadline end

--- myscript.js
+++ myscript.js
@@ -1,24 +1,24 @@
 
 $(document).ready(function() {
-    $("#submit").keypress(function (touchMouseKey) {
-        if ((touchMouseKey.which && touchMouseKey.which == 13) || (touchMouseKey.keyCode && touchMouseKey.keyCode == 13)) {
-            $('button[type=submit].default').click();
-            return false;
-        } else {
-            return true;
-        }
-    });
+  $("#submit").keypress(function (touchMouseKey) {
+    if ((touchMouseKey.which && touchMouseKey.which == 13) || (touchMouseKey.keyCode && touchMouseKey.keyCode == 13)) {
+      $('button[type=submit].default').click();
+      return false;
+    } else {
+      return true;
+    }
+  });
 });
 
-async function bookSearch(){
+async function bookSearch() {
   const search = await document.getElementById('search').value
   document.getElementById('results').innerHTML = "";
 
   $.ajax({
     url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
     dataType: "json",
-    success: function(data){
-      for(i = 0; i < data.items.length; i++){
+    success: function(data) {
+      for(i = 0; i < data.items.length; i++) {
         results.innerHTML += `<img class="flex" id="box-image" src=${data.items[i].volumeInfo.imageLinks.smallThumbnail}/>`
         results.innerHTML += `<h3 class="flex-text" id="box-text1">${data.items[i].volumeInfo.title}</h3>`
         results.innerHTML += `<h4 class="flex-text" id="box-text2">${data.items[i].volumeInfo.authors}</h4>`
