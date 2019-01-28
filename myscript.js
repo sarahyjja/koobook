3041bf78538319ec163ed6d93fb0de820c1103ca
sarah <sarahkharraz@gmail.com>
2019-01-28 21:06:02 +0000

    Form behavior NOK

--- myscript.js
+++ myscript.js
@@ -1,3 +1,15 @@
+
+$(document).ready(function() {
+    $("#submit").keypress(function (touchMouseKey) {
+        if ((touchMouseKey.which && touchMouseKey.which == 13) || (touchMouseKey.keyCode && touchMouseKey.keyCode == 13)) {
+            $('button[type=submit].default').click();
+            return false;
+        } else {
+            return true;
+        }
+    });
+});
+
 async function bookSearch(){
   const search = await document.getElementById('search').value
   document.getElementById('results').innerHTML = "";
@@ -11,7 +23,7 @@ async function bookSearch(){
         results.innerHTML += `<h3 class="flex-text" id="box-text1">${data.items[i].volumeInfo.title}</h3>`
         results.innerHTML += `<h4 class="flex-text" id="box-text2">${data.items[i].volumeInfo.authors}</h4>`
         results.innerHTML += `<h5 class="flex-text" id="box-text3">Published by { ${data.items[i].volumeInfo.publisher} }</h5>`
-        results.innerHTML += `<p class="flex-text" id="box-text4"><i>${data.items[i].volumeInfo.description}</i></p>`
+        results.innerHTML += `<p class="flex-text" id="box-text4">${data.items[i].volumeInfo.description}</p>`
         results.innerHTML += `<a href=${data.items[i].volumeInfo.infoLink} class="flex-text" id="box-text5">Read more...</a>`
 
       }
